510d0f82892bc3cdf5af58933c3da99db498a1b3
Dhaval Khatri <Dhaval.Khatri@einfochips.com>
2023-03-07 09:43:28 +0530

    First Framework

---
+++ tests/test_login.py
@@ -0,0 +1,26 @@
+from assertpy import assert_that
+from selenium.webdriver.common.by import By
+
+from base.webdriver_listener import WebDriverWrapper
+
+
+class TestLogin(WebDriverWrapper):
+    def test_valid_login(self):
+        self.driver.find_element(By.NAME, "username").send_keys("Admin")
+        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+        self.driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
+        actual_text = self.driver.find_element(By.XPATH, "//h6[normalize-space()='Dashboard']").text
+        assert_that("Dashboard").is_equal_to(actual_text)
+
+    def test_invalid_login(self):
+        pass
+
+
+class TestLoginUI(WebDriverWrapper):
+    def test_title(self):
+        actual_title = self.driver.title
+        assert_that("OrangeHRM").is_equal_to(actual_title)
+
+    def test_header(self):
+        actual_header = self.driver.find_element(By.XPATH, "//h5").text
+        assert_that("Login").is_equal_to(actual_header)
